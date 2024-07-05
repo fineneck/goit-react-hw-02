0e094ca35d406e04014107cae79dadd81678e8a2
OleksiiMikhieiev <babybong0993@gmail.com>
2024-07-05 11:55:19 +0300

    create components

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,12 @@
+// import css from "./Feedback.module.css";
+
+export default function Feedback() {
+  return (
+    <div>
+      <p>Good: </p>
+      <p>Neutral: </p>
+      <p>Bad: </p>
+      <p>Positive: </p>
+    </div>
+  );
+}
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,12 @@
+// import css from "./Options.module.css";
+
+export default function Options() {
+  return (
+    <div>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <button>Reset</button>
+    </div>
+  );
+}
